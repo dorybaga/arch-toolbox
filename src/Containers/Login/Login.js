0071c521dc935d59897d9bef5f80451c6248e5ee
Dory <dorybaga@gmail.com>
2017-10-14 15:35:22 -1000

    added schematic container; working on login form

--- src/Containers/Login/Login.js
+++ src/Containers/Login/Login.js
@@ -2,6 +2,25 @@ import React, { Component } from 'react';
 import './Login.css';
 
 class Login extends Component {
+  constructor() {
+    super();
+    this.state ={
+      userInput:''
+    };
+
+  }
+
+  userLogin(){
+    this.props.verfiyUser(this.state.userInput);
+  }
+
+  handleUserLogin(e){
+    console.log(e.target.value);
+    this.setState({
+      userInput: e.target.value
+    });
+  }
+
   render() {
     return (
       <div className="Login">
@@ -9,8 +28,22 @@ class Login extends Component {
           <h1 className="Login-title">Login</h1>
         </header>
 
+
         <form>
           <p>Please login</p>
+          <input
+            type="text"
+            placeholder="username"
+            onChange={this.handleUserLogin.bind(this)}
+           />
+           <br/>
+           <input
+            type="text"
+            placeholder="password"
+            onChange={this.handleUserLogin.bind(this)}
+           />
+           <br />
+           <button onClick={this.userLogin.bind(this)}>Log In</button>
         </form>
 
 
