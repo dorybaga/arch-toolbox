eb72e2a78f762af14d854705adb73114ff4be475
Dory <dorybaga@gmail.com>
2017-10-14 16:10:00 -1000

    created Header.js

---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+/*jshint esversion: 6 */
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div>
+      <h3>Foundation</h3>
+
+    </div>
+  )
+}
+
+export default Header;
--- src/Containers/Dashboard/Dashboard.js
+++ src/Containers/Dashboard/Dashboard.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Login from '../Login/Login.js';
 import './Dashboard.css';
+import Header from '../../Components/Header.js';
 
 class Dashboard extends Component {
   constructor() {
@@ -12,6 +13,7 @@ class Dashboard extends Component {
     return (
       <div className="Dashboard">
         <header className="Dashboard-header">
+          <Header />
           <h1 className="Dashboard-title">Project Dashboard</h1>
         </header>
       </div>
