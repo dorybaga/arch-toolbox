3068144b5ade46ef9ce1b5c25fdae585a2fb246e
Joshua Lee <jdl93@nau.edu>
2017-11-01 18:43:08 -1000

    pin is set

---
+++ src/containers/ModalContent/ModalContent.js
@@ -0,0 +1,28 @@
+/*jshint esversion: 6 */
+import React, { Component } from "react";
+
+class ModalContent extends Component {
+  constructor() {
+    super();
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div>
+       <h1>data and data and data</h1>
+       <h1>data and data and data</h1>
+       <h1>data and data and data</h1>
+       <h1>data and data and data</h1>
+       <h1>data and data and data</h1>
+       <h1>data and data and data</h1>
+       <h1>data and data and data</h1>
+       <h1>data and data and data</h1>
+      </div>
+    );
+  }
+}
+
+export default ModalContent;
