6c9900f56eb9e11630744d0c1c0fba84297b576c
Joshua Lee <jdl93@nau.edu>
2017-11-04 12:39:18 -1000

    sticky footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,32 @@
 /*jshint esversion: 6 */
 import React from "react";
+import {Card, CardActions, CardHeader, CardText} from 'material-ui/Card';
+import FlatButton from 'material-ui/FlatButton';
+import "../index.css";
+
+const style = {
+};
 
 const Footer = ({ project }) => {
   return (
     <div className="projectFooter">
-      <h3>{project.title}</h3>
-      <p>Job# {project.job_number}</p>
-      <p>Address: {project.address}</p>
-      <p>Client: {project.client_name}</p>
+      <Card>
+        <CardHeader
+          title="Project Details"
+          subtitle={project.title}
+          actAsExpander={true}
+          showExpandableButton={true}
+        />
+        <CardActions>
+          <FlatButton label="Photo Gallery" />
+          <FlatButton label="Comments" />
+        </CardActions>
+        <CardText expandable={true}>
+          <div>Project ID: {project.id}</div>
+          <div>Project Address: {project.address}</div>
+          <div>Project Client: {project.client_name}</div>
+        </CardText>
+      </Card>
     </div>
   );
 };
