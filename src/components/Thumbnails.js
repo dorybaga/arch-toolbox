236a881a5977ce68510f4f4f3321c0669929e9cc
Dory <dorybaga@gmail.com>
2017-11-09 20:15:15 -1000

    getPinPhoto() gets image from pin

---
+++ src/components/Thumbnails.js
@@ -0,0 +1,14 @@
+/*jshint esversion: 6 */
+
+import React from "react";
+
+const Thumbnails = () => {
+  return (
+    <div>
+      your saved images will render here
+      <img src="" />
+    </div>
+  );
+};
+
+export default Thumbnails;
--- src/containers/ModalContent/ModalContent.js
+++ src/containers/ModalContent/ModalContent.js
@@ -1,17 +1,42 @@
 /*jshint esversion: 6 */
 import React, { Component } from "react";
+import axios from "axios";
+
 import Camera from "../../components/Camera.js";
+// import CommentForm from "../Comments/CommentForm.js";
+import Thumbnails from "../../components/Thumbnails.js";
 
 class ModalContent extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+
+    this.state = {
+      pinImageUrl: ""
+    };
+  }
+
+  componentDidMount() {
+    this.getPinPhoto();
+  }
+
+  getPinPhoto() {
+    let projectId = localStorage.getItem("projectId");
+    let pinId = localStorage.getItem("pinId");
+    axios.get(`/api/projects/${projectId}/pin/${pinId}`).then(image => {
+      this.setState({
+        pinImageUrl: image.data[0].images[0].image_url
+      });
+    });
   }
 
   render() {
+    // console.log(this.state.pinImageUrl);
     return (
       <div>
+        <img src={this.state.pinImageUrl} />
+        <Thumbnails />
         <Camera />
+        {/* <CommentForm /> */}
       </div>
     );
   }
